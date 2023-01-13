72c69f784496ab030320e9d6df38f0a36bb48bf2
chi <C108112103@nkust.edu.tw>
2023-01-13 18:28:55 +0800

    no message

---
+++ code/q_learning.py
@@ -0,0 +1,77 @@
+# q_learning.py
+# -*- coding: UTF-8 -*-
+"""
+Q Learning Algorithm
+"""
+
+import numpy as np
+import pandas as pd
+import os
+
+
+class QLearning:
+    model_name = "Model\\model_final.csv"
+
+    def __init__(self, actions, learning_rate=0.01, discount_factor=0.9, e_greedy=0.1):
+        self.actions = actions  # action 列表
+        self.lr = learning_rate  # 學習速率
+        self.gamma = discount_factor  # 折扣因子
+        self.epsilon = e_greedy  # 貪婪
+        self.q_table = pd.DataFrame(columns=self.actions, dtype=np.float32)  # Q 表
+
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            self.q_table = self.q_table.append(
+                pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            )
+            flag = 0
+            return flag
+        else:
+            flag = 1
+            return flag
+
+    # 根據 state 來決定 action
+    def choose_action(self, state):
+        self.check_state_exist(state)  # 檢查當前 state 是否在 q_table 中存在
+        # 用 Epsilon Greedy 來選擇行為
+        if np.random.uniform() < self.epsilon:
+            # 隨機選 action
+            action = np.random.choice(self.actions)
+        else:  # 選擇 Q 直最高的 action
+            state_action = self.q_table.loc[state, :]
+
+            state_action = state_action.reindex(np.random.permutation(state_action.index))
+            action = state_action.idxmax()
+        return action
+
+    # 更新 Q 表中的值
+    def learn(self, s, a, r, s_):
+        flag = self.check_state_exist(s_)  # q_table 中是否存在 s_
+        # print('flag2')
+        # print(flag)
+
+        q_predict = self.q_table.loc[s, a]  # 根據 Q 表得到的 （predict）值
+        if s_ != 'terminal':  # state 不是 终止
+            q_target = r + self.gamma * self.q_table.loc[s_, :].max()
+        else:
+            q_target = r  # state 是 终止
+
+        # 更新 Q 表中 state_action 的數值
+        self.q_table.loc[s, a] += self.lr * (q_target - q_predict)
+
+    def q_table_save(self):
+        self.q_table.to_csv(self.model_name, index=True)
+        print('save data')
+        print(self.q_table)
+
+    def q_table_read(self):
+        if os.path.isfile(self.model_name):
+            self.q_table = pd.read_csv(self.model_name)
+            self.q_table.index
+            self.q_table.set_index("Unnamed: 0", inplace=True)
+        print(self.q_table)
+        print('read data')
---
+++ 分析/README.md
@@ -0,0 +1,13 @@
+# 分析
+## 分析(2022/11/09)
+![分析_20221109](https://user-images.githubusercontent.com/76472326/207598623-59f2e422-d6ae-4ddc-a798-cd0e105944fb.png)
+## 分析(2022/11/16)
+![分析_20221116](https://user-images.githubusercontent.com/76472326/207598646-ccc8e359-56e6-4206-bd2e-54b8a4c4eb4e.png)
+![分析_20221116_python端](https://user-images.githubusercontent.com/76472326/207598824-5baa4cf1-89bd-40f7-9b26-78b0a34591d5.png)
+![分析_20221116_unity端](https://user-images.githubusercontent.com/76472326/207598840-e74e8077-54ad-42e3-8c35-43486d2e1c22.png)
+## 分析(2022/11/30)
+![分析_20221130](https://user-images.githubusercontent.com/76472326/207598687-45d83a90-8500-43e5-80b0-dc51425490a4.png)
+## 分析(2022/12/07)
+![分析_20221207](https://user-images.githubusercontent.com/76472326/207598695-f995b435-c438-4551-aac6-bcd82722967b.png)
+## 分析(2023/01/11)
+![image](https://user-images.githubusercontent.com/76472326/212073334-436da2e1-5f44-4963-b293-ed6d1b04d317.png)
